b8ac5dfc11662db97fdf583c1deaf8b98b84191a
filmaj <maj.fil@gmail.com>
2017-03-29 09:49:18 -0700

    Track session ID using configstore within the CLI

--- spec/analytics.spec.js
+++ spec/analytics.spec.js
@@ -22,9 +22,13 @@ describe('PhoneGap Analytics', function() {
     });
     describe('trackEvent', function() {
         var post_spy;
+        var get_spy;
+        var set_spy;
         beforeEach(function() {
             spyOn(analytics, 'hasOptedOut').andReturn(false);
             post_spy = spyOn(request, 'post');
+            get_spy = spyOn(analytics.config, 'get');
+            set_spy = spyOn(analytics.config, 'set');
         });
         it('should POST to metrics.phonegap.com on successfully-parsed commands', function() {
             analytics.trackEvent(["platform", "list"]);
@@ -56,6 +60,48 @@ describe('PhoneGap Analytics', function() {
             analytics.trackEvent(cmd);
             var dump = JSON.parse(post_spy.calls[0].args[0].form);
             expect(dump._params).toContain('list');
+        });
+        describe('session tracking', function() {
+            it('should tag events with a session id', function() {
+                var cmd = ['plugins', 'list'];
+                analytics.trackEvent(cmd);
+                var dump = JSON.parse(post_spy.calls[0].args[0].form);
+                expect(dump._session).toEqual(jasmine.any(String));
+            });
+            it('should tag events with the same session ID if consecutive commands are issued within the event expiry time', function() {
+                get_spy.andCallFake(function(key) {
+                    if (key == 'lastRun') {
+                        return (new Date().valueOf() - analytics.EVENT_EXPIRY_TIME + 1);
+                    } else if (key == 'lastSession') {
+                        return "holygrail";
+                    }
+                });
+                var cmd = ['plugins', 'list'];
+                analytics.trackEvent(cmd);
+                var dump = JSON.parse(post_spy.calls[0].args[0].form);
+                expect(dump._session).toEqual("holygrail");
+            });
+            it('should tag events with a new session ID if consecutive commands are issued outside the event expiry time', function() {
+                get_spy.andCallFake(function(key) {
+                    if (key == 'lastRun') {
+                        return (new Date().valueOf() - (2 * analytics.EVENT_EXPIRY_TIME));
+                    } else if (key == 'lastSession') {
+                        return "holygrail";
+                    }
+                });
+                var cmd = ['plugins', 'list'];
+                analytics.trackEvent(cmd);
+                var dump = JSON.parse(post_spy.calls[0].args[0].form);
+                expect(dump._session).not.toEqual("holygrail");
+                expect(set_spy).toHaveBeenCalledWith('lastSession', jasmine.any(String));
+            });
+            it('should always keep track of the last run', function() {
+                var now = new Date().valueOf();
+                var cmd = ['plugins', 'list'];
+                analytics.trackEvent(cmd);
+                expect(set_spy).toHaveBeenCalledWith('lastRun', jasmine.any(Number));
+                expect(set_spy.calls[set_spy.calls.length - 1].args[1]).toEqual(now);
+            });
         });
     });
 });
