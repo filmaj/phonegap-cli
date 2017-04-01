6f9e7d0e4f761f5858f39fa6600ead974a7b33c0
filmaj <maj.fil@gmail.com>
2017-04-01 09:08:39 -0700

    the old push specs were copy-paste job from version module. axe those, and drop pending tests for now.

--- spec/phonegap/push.spec.js
+++ spec/phonegap/push.spec.js
@@ -7,33 +7,16 @@ var PhoneGap = require('../../lib/phonegap'),
     phonegap;
 
 /*
- * Specification: phonegap.version()
+ * Specification: phonegap.push()
  */
 
 describe('phonegap.push()', function() {
     beforeEach(function() {
         phonegap = new PhoneGap();
     });
-
-    it('should return a version object', function() {
-        expect(phonegap.version()).toEqual(jasmine.any(Object));
-    });
-
-    describe('version object', function() {
-        beforeEach(function() {
-            spyOn(fs, 'readFileSync').andReturn('{ "version": "2.8.0-0.10.6" }');
-        });
-
-        it('should contain version.npm', function() {
-            expect(phonegap.version().npm).toEqual('2.8.0-0.10.6');
-        });
-
-        it('should contain version.module', function() {
-            expect(phonegap.version().module).toEqual('0.10.6');
-        });
-
-        it('should contain version.phonegap', function() {
-            expect(phonegap.version().phonegap).toEqual('2.8.0');
-        });
-    });
+    it('should throw if no options are provided');
+    it('should throw if no payload is provided');
+    it('should use default phonegap push server endpoint if none provided');
+    it('should read from a payload file if provided in options');
+    it('should fire off a POST to the specified host on a successful call');
 });
