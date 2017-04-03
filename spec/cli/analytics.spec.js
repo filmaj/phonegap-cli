f6cfbd577a1a9c1f8d2b830b724636a965c92c3a
filmaj <maj.fil@gmail.com>
2017-04-02 21:18:29 -0700

    moar analytics specs (prompt method)

--- spec/cli/analytics.spec.js
+++ spec/cli/analytics.spec.js
@@ -2,10 +2,13 @@
  * Module dependencies.
  */
 
-var CLI = require('../../lib/cli'),
-    argv,
+var argv,
     cli,
-    stdout;
+    stdout,
+    rewire = require('rewire'),
+    analytics = rewire('../../lib/cli/analytics'),
+    CLI = require('../../lib/cli'),
+    request = require('request');
 
 /*
  * Specification: $ phonegap help analytics
@@ -81,6 +84,55 @@ describe('phonegap analytics', function() {
             var analytics_params = cli.analytics.calls.mostRecent().args[0]['_'];
             expect(analytics_params).toContain("analytics");
             expect(analytics_params).toContain("off");
+        });
+    });
+
+    describe('prompt', function() {
+        var permission_spy, track_spy, revert;
+        beforeEach(function() {
+            permission_spy = jasmine.createSpy();
+            track_spy = jasmine.createSpy();
+            revert = analytics.__set__('insight', {
+                'askPermission': permission_spy,
+                'trackEvent': track_spy
+            });
+            spyOn(request, 'post');
+        });
+        afterEach(function() {
+            revert(); // restore rewire clobbered globals
+        });
+        it('should ask permission to track analytics', function(done) {
+            permission_spy.and.callFake(function(msg, cb) {
+                cb(null, true);
+            });
+            analytics.prompt(function() {
+                expect(permission_spy).toHaveBeenCalled();
+                done();
+            });
+        });
+        it('should track opt in', function(done) {
+            permission_spy.and.callFake(function(msg, cb) {
+                cb(null, true);
+            });
+            analytics.prompt(function() {
+                var event = track_spy.calls.mostRecent().args[0];
+                expect(event.category).toEqual('analytics-prompt');
+                expect(event.action).toEqual('accepted');
+                expect(request.post).toHaveBeenCalled();
+                done();
+            });
+        });
+        it('should track opt out', function(done) {
+            permission_spy.and.callFake(function(msg, cb) {
+                cb(null, false);
+            });
+            analytics.prompt(function() {
+                var event = track_spy.calls.mostRecent().args[0];
+                expect(event.category).toEqual('analytics-prompt');
+                expect(event.action).toEqual('declined');
+                expect(request.post).toHaveBeenCalled();
+                done();
+            });
         });
     });
 });
